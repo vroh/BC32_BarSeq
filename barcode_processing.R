0a3e388f3d1259f0841166b1bd91da4331595cd4
vroh <roh.vincent@gmail.com>
2017-07-04 18:01:39 +0200

    Added quality score filtering variable Added new quality control tables and plots Updated changelog

--- barcode_processing.R
+++ barcode_processing.R
@@ -1,6 +1,6 @@
 ##################################################
 #                                                #
-#          Barcode Processing v.0.3              #
+#          Barcode Processing v.0.4              #
 #                                                #
 #               Roh et al. 2017                  #
 #                                                #
@@ -16,12 +16,15 @@ library(ShortRead)
 library(Biostrings)
 library(dplyr)
 library(stringdist)
+library(ggplot2)
+library(gridExtra)
 
 # For indels, an updated version of Biostrings' vmatchPattern is required (described in functions.R)
 source('functions.R')
 
 # Define variables
 pat <- 'CTANNCAGNNCTTNNCGANNCTANNCTTNNGGANNCTANNCAGNNCTTNNCGANNCTANNCTTNNGGANNCTANNCAGNN' # matching pattern (BC32)
+base_q <- 0 # minimum average base quality score in first 90 nucleotides
 bb_mis <- 1 # number of mismatch allowed in barcode backbone sequence
 indels <- 0 # total edit distance resulting from indels that are tolerated for barcode matching (barcodes with indels increase computation time)
 threshold <- 3 # threshold for Hamming distance in step pooling similar sequences
@@ -42,19 +45,24 @@ for (i in 1:length(seqfiles)) {
   #########################################
   
   # Read fastq file with ShortRead package
-  sequences <- readFastq(seqfiles[i])@sread
+  sequences <- readFastq(seqfiles[i])
+  init_len <- length(sequences) # collect for QC_stats
   name <- sampname$sample[i]
   message(paste('Processing sample: ', name))
+  
+  # Filter reads with average quality below base_q
+  good_qual <- (alphabetScore(quality(narrow(sequences, 1, 90)))/90) >= base_q
+  sequences <- sequences[good_qual]@sread
+  good_reads <- length(sequences)
   
   # Filter for sequences with specific multiplexing index (only keep sequences with the correct index, beware of 6 and 8-mer indexes!)
   # This steps avoids misassignment of samples at de-multiplexing 
   idx <- paste0('GTCAC', sampname$index[i], 'ATCTC')
   # Index matching (strict!)
   idx_match <- vmatchPattern(idx, sequences, 0)
-  # Subset sequences (and compute filtering ratio)
-  init_len <- length(sequences)
+  # Subset sequences
   sequences <- sequences[as.data.frame(idx_match)[,1]]
-  mis_idx_percent <- (init_len/length(sequences) - 1)*100
+  correct_index <- length(sequences) # collect for QC_stats
 
   # Grab barcode sequences
   # Trim 5' sequences
@@ -63,10 +71,12 @@ for (i in 1:length(seqfiles)) {
   match <- vmatchPattern(pat, sequences, length(strsplit(pat, 'N')[[1]]) + bb_mis)
   # Subset
   sequences_sub <- sequences[as.data.frame(match)[,1]]
+  match_no_indels <- length(sequences_sub) # collect for QC_stats
   left_out <- sequences[-as.data.frame(match)[,1]]
   # Extract barcode sequences from sequences subset
   match <- vmatchPattern(pat, sequences_sub, length(strsplit(pat, 'N')[[1]]) + bb_mis)
   barcodes <- sequences_sub[match]
+  match_with_indels <- 'sample processed without indel matching'
   if (indels) {
     # Remove sequences that will be matched by substitutions instead of indels (the total edit distance in vmatchPattern2 depends both on substitution and indels)
     substitution <- vmatchPattern(pat, left_out, max.mismatch = length(strsplit(pat, 'N')[[1]]) + bb_mis + indels, min.mismatch = length(strsplit(pat, 'N')[[1]]) + bb_mis + indels) # length(strsplit(pat, 'N')[[1]]) returns the count of N in the barcode sequence (32)
@@ -75,6 +85,7 @@ for (i in 1:length(seqfiles)) {
     match <- vmatchPattern2(pat, left_out, length(strsplit(pat, 'N')[[1]]) + bb_mis + indels, with.indels = T)
     # Subset
     indels_sub <- left_out[as.data.frame(match)[,1]]
+    match_with_indels <- length(indels_sub) # collect for QC_stats
     # Extract barcode sequences with indels
     match <- vmatchPattern2(pat, indels_sub, length(strsplit(pat, 'N')[[1]]) + bb_mis + indels, with.indels = T)
     barcodes <- c(barcodes, indels_sub[match])
@@ -88,9 +99,56 @@ for (i in 1:length(seqfiles)) {
   names(barcodes) <- 'seq'
   barcodes_summary <- count(barcodes, seq, sort=T)
   
-  # Write summary file
+  # Write summary file and QC_stats report
   write.table(barcodes_summary, paste0(name, '_barcode_summary.txt'), sep='\t', row.names = F)
-  write(mis_idx_percent, paste0(name, '_mis_idx.txt')) # This reports the percentage of sequences discarded by the index filtering
+  QC_stats <- rbind(c('Number of FASTQ sequences', init_len),
+                    c('Number of reads above quality threshold', good_reads),
+                    c('% good quality reads', 100*(good_reads/init_len)),
+                    c('Sequences matching multiplex index', correct_index),
+                    c('% correct index', 100*(correct_index/good_reads)),
+                    c('Barcodes match (without indels)', match_no_indels),
+                    c('% without indels', 100*(match_no_indels/correct_index)),
+                    c('Barcodes matching (with indels)', match_with_indels),
+                    c('% with and without indels', 100*(match_no_indels + ifelse(indels, match_with_indels, 0))/correct_index))
+  write.table(QC_stats, paste0(name, '_QC_stats.xls'), sep = '\t', row.names = F, col.names = F)
+  
+  # Export QC plots
+  # prepare data frame
+  QC_plot <- as.data.frame(QC_stats[c(1,2,4),])
+  QC_plot$V2 <- as.numeric(as.character(QC_plot$V2))
+  # export plots as pdf
+  pdf(paste0(name, '_QC_plots.pdf'), 10)
+  # plotA: sequences after quality filtering and index matching
+  plotA <- ggplot(QC_plot, aes(V1, V2)) +
+    geom_bar(stat = 'identity') +
+    ylim(0, max(QC_plot$V2)) +
+    xlab("") +
+    ylab("Count") +
+    theme(axis.ticks.x = element_blank(),
+          axis.text.x = element_blank()) +
+    geom_label(aes(x = 1, y = 0, label = paste0('n = ', QC_stats[1,2]))) +
+    geom_label(aes(x = 2, y = 0, label = paste0('% = ', round(as.numeric(QC_stats[3,2]), 2)))) +
+    geom_label(aes(x = 3, y = 0, label = paste0('% = ', round(as.numeric(QC_stats[5,2]), 2)))) +
+    geom_label(aes(x = 1, y = max(QC_plot$V2), label = 'FASTQ sequences')) +
+    geom_label(aes(x = 2, y = max(QC_plot$V2), label = 'Good quality')) +
+    geom_label(aes(x = 3, y = max(QC_plot$V2), label = 'Correct index'))
+  # plotB: sequences after barcode matching
+  QC_plot_indels <- as.data.frame(cbind(c('substitutions', 'indels'),
+                                        c(100*(as.numeric(as.character(QC_stats[6,2]))/as.numeric(as.character(QC_stats[4,2]))),
+                                          100*(as.numeric(as.character(QC_stats[8,2]))/as.numeric(as.character(QC_stats[4,2]))))))
+  QC_plot_indels$V2 <- as.numeric(as.character(QC_plot_indels$V2))
+  plotB <- ggplot(QC_plot_indels, aes(1, V2)) +
+    geom_bar(stat = 'identity', aes(fill = V1)) +
+    ylim(0, 100) +
+    xlab("") +
+    ylab("% sequences") +
+    theme(axis.ticks.x = element_blank(),
+          axis.text.x = element_blank()) +
+    scale_fill_discrete(guide_legend(title = 'matching group')) +
+    geom_label(aes(x = 1, y = 0, label = paste0('% = ', round(as.numeric(QC_stats[9,2]), 2)))) +
+    geom_label(aes(x = 1, y = 100, label = 'Barcode matching'))
+  grid.arrange(plotA, plotB, nrow = 1)
+  dev.off()
   
   # 1.b. Pool similar barcodes together (according to Hamming distance)
   #####################################################################
